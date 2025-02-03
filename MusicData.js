b55dbd683a7b9afa31bafe831a58922697bbd5c7
ngo <ngobodyy@gmail.com>
2025-02-03 07:33:08 -0500

    defined all classes and printed in html and console

--- MusicData.js
+++ MusicData.js
@@ -2,22 +2,47 @@
 
 
 //1. Declare a class called Song, it's constructor should take three parameters: title, artist, album
-
-
+class Song {
+    constructor (title, artist, album){
+        this.title = title;
+        this.artist = {
+            name: artist
+        };
+        this.album = album;
+    }
+};
 
 //2. Declare a class called Musician, it's constructor should take three parameters: name, instrument, genre
-
-
+class Musician {
+    constructor (name, instrument, genre){
+        this.name = name;
+        this.instrument = instrument;
+        this.genre = genre;
+    }
+};
 
 //3. Declare a class called Playlist, it's constructor should take four parameters: name, song1, song2, song3, song4
+class Playlist {
+    constructor (name, song1, song2, song3){
+        this.name = name;
+        this.song1 = song1;
+        this.song2 = song2;
+        this.song3 = song3;
+    }
 //4. this class should also define a method called getInfo() that prints text to the console saying
 //  The playlist (whatever name you gave it) has the following songs:
 //         (Song1 name) by (Song1 Artist Name),
 //         (Song2 name) by (Song2 Artist Name),
 //         (Song3 name) by (Song3 Artist Name),
-
+    getInfo(){
+        console.log(`The playlist ${this.name} has the following songs:`);
+        console.log(`${this.song1.title} by ${this.song1.artist.name}`);
+        console.log(`${this.song2.title} by ${this.song2.artist.name}`);
+        console.log(`${this.song3.title} by ${this.song3.artist.name}`);
+    }
+}
 
 
 //5. export all three classes to make them available to the other JS files.
 
-
+export { Song, Musician, Playlist }
--- script.js
+++ script.js
@@ -1,38 +1,29 @@
 // 1. Import the all three objects (Song, Musician, Playlist) that you created in the MusicData.js module file
-
+import { Song, Musician, Playlist } from "/MusicData.js";
 
 // 2. declare a new constant and assign to it a new musician object to describe the musician for the first song.
-
-
-
+const Musician1 = new Musician("Charli xcx", "Synthesizer", "Hyperpop");
 // 3. declare a new constant and assign to it a new musician object to describe the musician for the second song.
-
-
-
-
+const Musician2 = new Musician("Foster the People", "Synthesizer, Drums, Guitar", "Indie Pop");
 // 4. declare a new constant and assign to it a new musician object to describe the musician for the third song.
-
-
+const Musician3 = new Musician("Death Cab for Cutie", "Guitar, Drums", "Indie Pop");
 // 5. declare a new constant and assign to it a new Song object to describe the first song
 // (make sure to use the Musician object from step 2 in creating this Song object
-
-
-
+const Song1 = new Song("c2.0", `${Musician1.name}`, "how i'm feeling now");
 // 6. declare a new constant and assign to it a new Song object to describe the second song
 // (make sure to use the Musician object from step 3 in creating this Song object
-
-
+const Song2 = new Song("Helena Beat", `${Musician2.name}`, "Torches");
 // 7. declare a new constant and assign to it a new Song object to describe the third song
 // (make sure to use the Musician object from step 4 in creating this Song object
-
-
+const Song3 = new Song("Soul Meets Body", `${Musician3.name}`, "Plans");
 // 8. declare a new constant called myPlaylist and assign to it a new Playlist object and make sure to use the three Song objects
 //from steps 5, 6, & 7 when creating it.
 //It must be called myPlaylist
 //It must be called myPlaylist
-
+const myPlaylist = new Playlist("my partner's playlist :D", Song1, Song2, Song3);
 
 //9. call the .getInfo() method on myPlaylist
+myPlaylist.getInfo();
 
 
 //DO NOT EDIT BELOW THIS LINE
